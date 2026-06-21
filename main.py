bbaed552065f4d2f1751a8d865793a84e6322365
Айдын Бабаев <ajdynbabaev@MacBook-Air-Ajdyn.local>
2026-06-21 15:31:08 +0400

    test 2 hour reminders

--- main.py
+++ main.py
@@ -88,8 +88,8 @@ time_keyboard = ReplyKeyboardMarkup(
     [
         ["🕙 10:00", "🕚 11:00"],
         ["🕛 12:00", "🕐 13:00"],
-        ["🕑 14:00", "🕒 15:00"]
-        ["🕑 16:00", "🕒 17:00"]
+        ["🕑 14:00", "🕒 15:00"],
+        ["🕑 16:00", "🕒 17:00"],
         ["🕑 18:00", "🕒 19:00"]
     ],
     resize_keyboard=True
@@ -141,6 +141,10 @@ def clean_time(text):
         .replace("🕐 ", "")
         .replace("🕑 ", "")
         .replace("🕒 ", "")
+        .replace("🕓 ", "")
+        .replace("🕔 ", "")
+        .replace("🕕 ", "")
+        .replace("🕖 ", "")
         .strip()
     )
 
